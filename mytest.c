001276bad4e23bc9019daf11e20edf29f975297d
haakonsbakken <haakonsbakken@iot-labs.no>
2019-03-20 08:28:59 +0100

    remove pullup

--- mytest.c
+++ mytest.c
@@ -8,11 +8,11 @@ int main()
     if (!bcm2835_init())
         return 1;
 
-    printf("Let's try to read an input with the bcm2835 library!");
+    printf("Reading GPIO21 with the bcm2835 library..\n");
     // set RPI pin to be an input
     bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_INPT);
     // with a pullup
-    bcm2835_gpio_set_pud(PIN, BCM2835_GPIO_PUD_UP);
+    //bcm2835_gpio_set_pud(PIN, BCM2835_GPIO_PUD_UP);
     while (1)
     {
         // Read some data
